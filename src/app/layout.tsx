691562ea1928cc36d722002ea14a160e3df3eee2
aliirz <dejavu87@gmail.com>
2026-03-17 11:07:59 +0300

    added v analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist_Mono } from "next/font/google";
 import { EasterEggs } from "@/components/EasterEggs";
 import "./globals.css";
-
+import { Analytics } from "@vercel/analytics/next"
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
